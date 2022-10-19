8f401cbf2cddd7847d5c52dc0879f256d99b8039
SSparks31 <mochizuki@belzeen.net>
2022-10-19 17:19:51 -0300

    Better path handling

--- T3/src/qry.c
+++ T3/src/qry.c
@@ -4,46 +4,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <linux/limits.h>
 
 #include "list.h"
 
 #include "svg.h"
 #include "data.h"
 
+
 void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree shapes) {    
-    if (!qry_name || strlen(qry_name) == 0) {
-        return;
-    }
-
-    char* qry_path = calloc(1, strlen(BED) + 1 + strlen(qry_name) + 1);
-    if (BED[strlen(BED)-1] != '/') {
-        sprintf(qry_path, "%s/%s", BED, qry_name);
-    } else {
-        sprintf(qry_path, "%s%s", BED, qry_name);
-    }
-
+    char qry_path[PATH_MAX];
+    sprintf(qry_path, "%s%s%s", BED, BED[strlen(BED) - 1] == '/' ? "" : "/", qry_name);
     FILE* qry_file = fopen(qry_path, "r");
-    if (!qry_file) {
-        return;
-    }
+
+    char output_path[PATH_MAX];
+    *strrchr(geo_name, '.') = '\0';
 
     if (strrchr(geo_name, '/')) {
         geo_name = strrchr(geo_name, '/') + 1;
     }
-    qry_name = strrchr(qry_path, '/') + 1;
-    *strrchr(geo_name, '.') = '\0';
-    *strrchr(qry_name, '.') = '\0';
-
-    char* svg_path = calloc(1, strlen(BSD) + 1 + strlen(geo_name) + 1 + strlen(qry_name) + 4 + 1);
-
-    if (BSD[strlen(BSD)-1] != '/') {
-        sprintf(svg_path, "%s/%s-%s.svg", BSD, geo_name, qry_name);
-    } else {
-        sprintf(svg_path, "%s%s-%s.svg", BSD, geo_name, qry_name);
-    }
     
-    FILE* svg_file = fopen(svg_path, "w");
-    free(svg_path);
+    if (strrchr(qry_name, '/')) {
+        qry_name = strrchr(qry_name, '/') + 1;
+    }
+
+    sprintf(output_path, "%s%s%s-%s", BSD, BSD[strlen(BSD) - 1] == '/' ? "" : "/", geo_name, qry_name);
+    sprintf(strrchr(output_path, '.'), ".svg"); 
+    FILE* svg_file = fopen(output_path, "w");
+
+    sprintf(strrchr(output_path, '.'), ".txt"); 
+    FILE* txt_file = fopen(output_path, "w");
 
     fprintf(svg_file, "<svg xmlns=\"http://www.w3.org/2000/svg\">\n");
     
@@ -60,17 +50,13 @@ void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree sh
         } else if (strcmp(command, "mc") == 0) {
             
         } else {
-            printf("Comando nao reconhecido\n");
+            printf("Comando %s nao reconhecido\n", command);
             fgets(command, 999, qry_file);
         }
     }
-    
-    fprintf(svg_file, "</svg>");
 
-    fclose(qry_file);
-    fclose(svg_file);
-
-    free(qry_path);
+    sprintf(strrchr(output_path, '.'), ".dot"); 
+    printSRB(shapes, output_path);
 
     return;
 }
