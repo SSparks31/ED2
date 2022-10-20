d6d1abc282c0333329e04b0eba804a0b225d39cd
SSparks31 <mochizuki@belzeen.net>
2022-10-20 00:50:18 -0300

    E

--- T3/src/qry.c
+++ T3/src/qry.c
@@ -11,8 +11,33 @@
 #include "svg.h"
 #include "data.h"
 
+struct eInfo {
+    double energy;
+    FILE* txt_file;
+};
 
-void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree shapes) {    
+void energize(SRBTree_elem i, double x, double y, double mbbX1, double mbbY1, double mbbX2, double mbbY2, void *aux) {
+    Shape shape = data_get_shape(i);
+    struct eInfo* info = aux;
+    if (shape_get_type(shape) == SVG_RECTANGLE) {
+        data_set_value(i, info->energy);
+        fprintf(info->txt_file, "\tNau %d energizada em %.2lf\n", shape_get_id(shape), info->energy);
+    }
+}
+
+void e(FILE* qry_file, FILE* txt_file, SRBTree sea) {
+    fprintf(txt_file, "=== E ===\n");
+    
+    double energy;
+    fscanf(qry_file, " %lf\n", &energy);
+    struct eInfo info;
+    info.energy = energy;
+    info.txt_file = txt_file;
+    percursoLargura(sea, energize, &info);
+    fprintf(txt_file, "\n");
+}
+
+void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree sea) {    
     char qry_path[PATH_MAX];
     sprintf(qry_path, "%s%s%s", BED, BED[strlen(BED) - 1] == '/' ? "" : "/", qry_name);
     FILE* qry_file = fopen(qry_path, "r");
@@ -40,7 +65,7 @@ void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree sh
     char command[999];
     while (fscanf(qry_file, "%s", command) != EOF) {
         if (strcmp(command, "e") == 0) {
-            
+            e(qry_file, txt_file, sea);
         } else if (strcmp(command, "mv") == 0) {
             
         } else if (strcmp(command, "lr") == 0) {
@@ -55,8 +80,13 @@ void qry_parser(char* BED, char* BSD, char* geo_name, char* qry_name, SRBTree sh
         }
     }
 
+    fprintf(svg_file, "</svg>");
+
     sprintf(strrchr(output_path, '.'), ".dot"); 
-    printSRB(shapes, output_path);
+    printSRB(sea, output_path);
+
+    fclose(txt_file);
+    fclose(svg_file);
 
     return;
 }
